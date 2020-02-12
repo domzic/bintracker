81d9c0b2287abdcd49d4831092117f76ba727936
Domas Žičevičius <dzicevicius@kayak.com>
2020-02-12 21:50:52 +0200

    Google maps integration and simple map display

---
+++ client/src/components/map/map.styles.jsx
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+
+export const MapWrapper = styled.div`
+    width: 50vw;
+    height: 50vh;
+`;
--- index.js
+++ index.js
@@ -1,19 +1,32 @@
 const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+require('dotenv').config();
+
 
 const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ 
+  extended: true 
+}));
+
+app.use(cors());
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
+    app.use(express.static(path.join(__dirname, 'client/build')));
 
-  const path = require('path');
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-};
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
+    });
+}
 
-app.get('/', (req, res) => {
-    res.send('Hello world');
+app.listen(port, error => {
+    if (error) {
+        throw error;
+    };
+    console.log('Server running on port ' + port);
 });
-
-const PORT = process.env.PORT || 5000;
-app.listen(PORT);
