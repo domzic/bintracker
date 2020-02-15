3879dfd663e351342fc819afd9f8339721bdf16d
Domas Žičevičius <dzicevicius@kayak.com>
2020-02-15 13:06:16 +0200

    Simplify google oauth user handler

--- controllers/userController.js
+++ controllers/userController.js
@@ -23,14 +23,9 @@ passport.use(
       proxy: true
     },
     async (accessToken, refreshToken, profile, done) => {
-      const existingUser = await User.findOne({ googleId: profile.id });
-      if (existingUser) {
-        return done(null, existingUser);
-      }
-      const user = await new User({ 
-          googleId: profile.id 
-      }).save();
-      done(null, user);
+        User.findOrCreate({ googleId: profile.id }, function (err, user) {
+            return done(err, user);
+        });
     }
   )
 );
