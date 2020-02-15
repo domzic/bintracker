e6597583975b1035853255ac853cfaea0ed29f67
Domas Žičevičius <dzicevicius@kayak.com>
2020-02-15 13:03:18 +0200

    Authentication backend with google oauth and passport

--- index.js
+++ index.js
@@ -1,20 +1,37 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const path = require('path');
+require('./models/User');
 
 require('dotenv').config();
 
+mongoose.connect(process.env.MONGO_URI, { 
+    useNewUrlParser: true,
+    useUnifiedTopology: true 
+});
 
 const app = express();
 const port = process.env.PORT || 5000;
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ 
   extended: true 
 }));
 
+app.use(
+    cookieSession({
+      maxAge: 30 * 24 * 60 * 60 * 1000,
+      keys: [process.env.COOKIE_KEY]
+    })
+  );
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(cors());
+
+require('./routes/authRoutes')(app);
 
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, 'client/build')));
