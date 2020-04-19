38c455dc9589d8fe775e9ac1fb6dd5932851cf1e
domzic <domas.zicevicius@gmail.com>
2020-04-19 12:52:07 +0300

    Fix emploee deletion confirmation popup

--- client/src/components/employees-list/employees-list.component.jsx
+++ client/src/components/employees-list/employees-list.component.jsx
@@ -1,4 +1,4 @@
-import React, {useContext, forwardRef, useState} from 'react';
+import React, { useContext, forwardRef, useState } from 'react';
 import axios from 'axios';
 import MaterialTable from 'material-table';
 import './employees-list.styles.css';
@@ -19,32 +19,42 @@ import SaveAlt from '@material-ui/icons/SaveAlt';
 import Search from '@material-ui/icons/Search';
 import ViewColumn from '@material-ui/icons/ViewColumn';
 import { Context } from '../../state/store';
-import Dialog from "@material-ui/core/Dialog";
-import DialogTitle from "@material-ui/core/DialogTitle";
-import DialogContent from "@material-ui/core/DialogContent";
-import DialogContentText from "@material-ui/core/DialogContentText";
-import DialogActions from "@material-ui/core/DialogActions";
-import Button from "@material-ui/core/Button";
-import Slide from "@material-ui/core/Slide";
+import Dialog from '@material-ui/core/Dialog';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogActions from '@material-ui/core/DialogActions';
+import Button from '@material-ui/core/Button';
+import Slide from '@material-ui/core/Slide';
+import { makeStyles } from '@material-ui/core/styles';
+import { useFormButtonStyles } from '../utils/mui-styles';
 
 export const tableIcons = {
     Add: forwardRef((props, ref) => <AddBox {...props} ref={ref} />),
     Check: forwardRef((props, ref) => <Check {...props} ref={ref} />),
     Clear: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
     Delete: forwardRef((props, ref) => <DeleteOutline {...props} ref={ref} />),
-    DetailPanel: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
+    DetailPanel: forwardRef((props, ref) => (
+        <ChevronRight {...props} ref={ref} />
+    )),
     Edit: forwardRef((props, ref) => <Edit {...props} ref={ref} />),
     Export: forwardRef((props, ref) => <SaveAlt {...props} ref={ref} />),
     Filter: forwardRef((props, ref) => <FilterList {...props} ref={ref} />),
     FirstPage: forwardRef((props, ref) => <FirstPage {...props} ref={ref} />),
     LastPage: forwardRef((props, ref) => <LastPage {...props} ref={ref} />),
     NextPage: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
-    PreviousPage: forwardRef((props, ref) => <ChevronLeft {...props} ref={ref} />),
+    PreviousPage: forwardRef((props, ref) => (
+        <ChevronLeft {...props} ref={ref} />
+    )),
     ResetSearch: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
     Search: forwardRef((props, ref) => <Search {...props} ref={ref} />),
-    SortArrow: forwardRef((props, ref) => <ArrowDownward {...props} ref={ref} />),
-    ThirdStateCheck: forwardRef((props, ref) => <Remove {...props} ref={ref} />),
-    ViewColumn: forwardRef((props, ref) => <ViewColumn {...props} ref={ref} />)
+    SortArrow: forwardRef((props, ref) => (
+        <ArrowDownward {...props} ref={ref} />
+    )),
+    ThirdStateCheck: forwardRef((props, ref) => (
+        <Remove {...props} ref={ref} />
+    )),
+    ViewColumn: forwardRef((props, ref) => <ViewColumn {...props} ref={ref} />),
 };
 
 const Transition = forwardRef(function Transition(props, ref) {
@@ -52,24 +62,30 @@ const Transition = forwardRef(function Transition(props, ref) {
 });
 
 const EmployeesList = () => {
-
+    const buttonClasses = useFormButtonStyles();
     const { company } = useContext(Context)[0];
-    const [dialogState, setDialogState] = useState({ opened: false, selected: null });
+    const [dialogState, setDialogState] = useState({
+        opened: false,
+        selected: null,
+    });
 
     const tableData = {
         columns: [
             { title: 'Display name', field: 'displayName' },
-            { title: 'Email', field: 'email' }
+            { title: 'Email', field: 'email' },
         ],
-        data: company.employees
+        data: company.employees,
     };
 
     const handleClose = () => {
-        setDialogState( { selected: null, opened: false })
+        setDialogState({ selected: null, opened: false });
     };
 
     const onConfirm = () => {
-        axios.delete('/api/company/employee', { data: { employeeEmail: dialogState.selected } })
+        axios
+            .delete('/api/company/employee', {
+                data: { employeeEmail: dialogState.selected },
+            })
             .then(() => window.location.reload());
     };
 
@@ -84,8 +100,12 @@ const EmployeesList = () => {
                     {
                         icon: DeleteOutline,
                         tooltip: 'Delete Employee',
-                        onClick: (event, employee) => setDialogState({ selected: employee.email, opened: true})
-                    }
+                        onClick: (event, employee) =>
+                            setDialogState({
+                                selected: employee.email,
+                                opened: true,
+                            }),
+                    },
                 ]}
             />
             <Dialog
@@ -96,22 +116,50 @@ const EmployeesList = () => {
                 aria-labelledby="alert-dialog-slide-title"
                 aria-describedby="alert-dialog-slide-description"
             >
-                <DialogTitle id="alert-dialog-slide-title">{"Confirmation"}</DialogTitle>
+                <DialogTitle id="alert-dialog-slide-title">
+                    {'Confirmation'}
+                </DialogTitle>
                 <DialogContent>
                     <DialogContentText id="alert-dialog-slide-description">
-                        Are you sure you want to delete this employee? You won't be able to undo this action.
+                        Are you sure you want to delete this employee? You won't
+                        be able to undo this action.
                     </DialogContentText>
                 </DialogContent>
-                <DialogActions>
-                    <Button onClick={handleClose} color="secondary">
+                <DialogActions style={{ justifyContent: 'space-between' }}>
+                    <Button
+                        classes={makeStyles({
+                            root: {
+                                borderColor: '#FFAAAA',
+
+                                '&:hover': {
+                                    borderColor: '#FF5D5D',
+                                },
+                            },
+                            label: {
+                                color: '#fff',
+                                '&:hover': {
+                                    color: '#FFAAAA',
+                                },
+                            },
+                        })()}
+                        onClick={handleClose}
+                        variant="outlined"
+                        color="primary"
+                        className="cancel"
+                    >
                         Cancel
                     </Button>
-                    <Button onClick={onConfirm} color="primary">
+                    <Button
+                        onClick={onConfirm}
+                        classes={buttonClasses}
+                        variant="outlined"
+                        color="primary"
+                    >
                         Confirm
                     </Button>
                 </DialogActions>
             </Dialog>
-      </div>
+        </div>
     );
 };
 
